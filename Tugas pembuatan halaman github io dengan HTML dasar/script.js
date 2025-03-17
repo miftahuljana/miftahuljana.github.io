b461205a932a502e88da1bcca7b66ae27bc02d71
miftahul <miftajanahhul@gmail.com>
2025-03-17 23:14:56 +0800

    Add files via upload

---
+++ Tugas pembuatan halaman github io dengan HTML dasar/script.js
@@ -0,0 +1,7 @@
+// Fungsi untuk mengganti warna tema
+document.addEventListener("DOMContentLoaded", function () {
+    let footer = document.querySelector(".footer");
+    footer.addEventListener("click", function () {
+        alert("Terima kasih telah melihat CV saya!");
+    });
+});
